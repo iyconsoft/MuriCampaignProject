920922c237ceba16d2fc65bbe5ac4aa3953c9992
adeel696 <adeel6962yahoo.com>
2023-09-03 04:55:02 +0500

    Fix name 4

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,4 +13,5 @@ class Cors
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
+	}
 }
